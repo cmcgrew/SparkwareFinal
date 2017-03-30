94420ca83f2141093714e85e7e7dd94d6daf1e63
Abdullah Ajmeri <aajmeri@asu.edu>
2017-03-30 10:40:53 -0700

    First badge works. Need to add validation for user and save the badge.

--- SparkwareFinal/Fragments/SubmitIdeaFragment.cs
+++ SparkwareFinal/Fragments/SubmitIdeaFragment.cs
@@ -12,21 +12,51 @@ using Android.Views;
 using Android.Widget;
 using Android.Support.V4.App;
 
+using Xamarin.Android;
+
 namespace SparkwareFinal.Fragments
 {
     public class SubmitIdeaFragment : Android.Support.V4.App.Fragment
     {
+        
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
+                
+             
+        
+            
             // Create your fragment here
+
         }
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             View view = inflater.Inflate(Resource.Layout.submitidea_page, container, false);
+            
+            Button submitIdeaButton = view.FindViewById<Button>(Resource.Id.btnSubmitFeedback);
+            submitIdeaButton.Click += SubmitIdeaButon_Click;
+            
             return view;
+
+        }
+
+        private void SubmitIdeaButon_Click(object sender, EventArgs e)
+        {
+           // ImageView badge1 = new ImageView(Activity);
+            //badge1  
+            AlertDialog.Builder alert = new AlertDialog.Builder(Activity);
+            AlertDialog myAlert = alert.Create();
+            myAlert.SetTitle("Badge Earned!");
+            myAlert.SetMessage("First feedback badge earned!");
+            myAlert.SetIcon(Resource.Drawable.badge1);
+            myAlert.SetButton("Awesome!", (s, ev) =>
+            {
+                Toast.MakeText(Activity, "Badge Earned!", ToastLength.Long).Show();
+            });
+
+            myAlert.Show();
         }
     }
+
 }
